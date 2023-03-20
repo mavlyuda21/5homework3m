6c752b01ddc9b80b3091beeadf7ed9d6094aa986
mavlyuda <sashakilka1991.@gmail.com>
2023-03-21 01:09:42 +0600

    Initial Commit

---
+++ 5homework3m/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5homework3m
+//
+//  Created by mavluda on 21/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
